eaee9b1c10ec37ed77b366befd7cba0635371eca
Kseniya <pashkina.ks@mail.ru>
2022-09-18 18:07:01 +0700

    2

---
+++ 2/Program.cs
@@ -0,0 +1,26 @@
+﻿// for
+string Metod4(int count, string text)
+{
+string result = String.Empty;
+for (int i = 0; i < count; i++) // - вначале ключевое слово, затем инициализация
+//счётчика, после проверка условия и инкремент (увеличение счётчика).
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Metod4(10, "asd");
+// Console.WriteLine(res);
+
+
+// цикл в цикле
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
